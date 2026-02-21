bc17bc62bc0987b40065b9dd399778d29cff3a84
Stepan Zhilenkov <stepweister@gmail.com>
2026-02-21 18:33:11 +0300

    fix main3_test.go

--- 3/main3_test.go
+++ 3/main3_test.go
@@ -10,17 +10,25 @@ func TestMap(t *testing.T) {
 	if !ok || val != 10 {
 		t.Error("Ошибка: не удалось добавить или получить Gopher")
 	}
-	if res.Exists("Gopher") {
+
+	if !res.Exists("Gopher") {
 		t.Error("Ошибка: должен был вернуть true")
 	}
+
 	copyData := res.Copy()
 	if copyData["Gopher"] != 10 {
 		t.Error("Ошибка: в копии нет данных из оригинала")
 	}
+	res.Add("Gopher", 999)
+	if copyData["Gopher"] == 999 {
+		t.Error("Ошибка: копия изменилась вместе в оригиналом (это не копия!)")
+	}
+
 	res.Remove("Gopher")
 	if res.Exists("Gopher") {
 		t.Error("Ошибка: Gopher все еще существует после удаления")
 	}
+
 	val, ok = res.Get("Gopher")
 	if ok || val != 0 {
 		t.Error("Ошибка: Get должен возвращать (0, false) для удаленного ключа")
